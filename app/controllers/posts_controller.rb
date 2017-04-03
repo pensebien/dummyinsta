f197db89b6e3e24785d120762405c103843b89f8
EmmaAkin <akinpense2@gmail.com>
2017-04-03 23:30:17 +0200

    Feature:             Post
                The timeline shows features starting from the
                bottom
    
                When a user enter the route for the
                posts display he see the post in a descending order
    
                Modified to show the post in an ascending order

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
     def index
-        @posts = Post.all
+        @posts = Post.all.order(created_at: :desc)
     end
 
     def new
@@ -9,6 +9,10 @@ class PostsController < ApplicationController
 
     end
 
+    def show
+        @post = Post.find(params[:id])
+
+    end
 
     def create
 
