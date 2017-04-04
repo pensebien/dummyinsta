f793e6338849c3e99f18714ea1587249d541271b
EmmaAkin <akinpense2@gmail.com>
2017-04-04 14:32:37 +0200

    Adding Testing to the application

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,52 +1,68 @@
 class PostsController < ApplicationController
 
-    def index
-        @posts = Post.all.order(created_at: :desc)
-    end
+            before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-    def new
-        @post = Post.new
+            def index
+                @posts = Post.all.order(created_at: :desc)
+            end
 
-    end
+            def new
+                @post = Post.new
 
-    def show
-        @post = Post.find(params[:id])
+            end
 
-    end
+            def show
 
-    def edit
-        @post = Post.find(params[:id])
-    end
+            end
 
-    def update
-        @post = Post.find(params[:id])
-        @post.update(post_params)
+             def create
+                if @post = Post.create(post_params)
+                    flash[:success] = "Your post has been created!"
 
-        redirect_to(posts_path(@post))
+                    redirect_to posts_path
+                else
+                    flash.now[:alert] = "Your new post could not created! Try again"
 
-    end
+                    render :new
+                end
+            end
 
-    def create
+            def edit
 
-        @post = Post.create(post_params)
+            end
 
-        redirect_to posts_path
+            def update
+                if @post.update(post_params)
+                     flash[:success] = "Update succesfful!"
 
-    end
+                    redirect_to posts_path
+                else
+                    flash.now[:alert] = "Update failed! Try to check the form again"
 
-
-    def destroy
-        @post = Post.find(params[:id])
-        @post.destroy
-
-        redirect_to posts_path
-    end
-
-end
+                    render :edit
+                end
+            end
 
 
-private
 
-def post_params
-  params.require(:post).permit(:image, :content)
+
+            def destroy
+                @post.destroy
+
+                redirect_to root_path
+            end
+
+
+
+
+            private
+
+            def set_post
+                @post = Post.find(params[:id])
+            end
+
+            def post_params
+              params.require(:post).permit(:image, :content)
+            end
+
 end
